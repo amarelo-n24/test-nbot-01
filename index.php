984c63b6afd7f154cbec60907562f2d9b80f105a
amarelo-n24 <47935239+amarelo-n24@users.noreply.github.com>
2019-03-03 20:12:20 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
-  echo "hello world";
+  //composerでインストールしたライブラリを一括読み込み
+  require_once __DIR__ . '/vendor/autoload.php';
+
+  //POSTメゾットで渡される値を取得、表示
+  $inputString = file_get_contents('php://input');
+  error_log($inputString);
 ?>
