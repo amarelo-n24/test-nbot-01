e446b0f8069b8ec773b8c387c5961b1711a7e246
amarelo-n24 <47935239+amarelo-n24@users.noreply.github.com>
2019-03-06 00:13:43 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
   //LINE Messaging APIがリクエストに付与した署名を取得
   $signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
   //署名が正当かチェック。正当であればリクエストをパースし配列へ
-  $events = $bot->parseEventRequest(file_get_contents('php://input'),$singature);
+  $events = $bot->parseEventRequest(file_get_contents('php://input'),$signature);
   //配列に格納された各イベントをループ処理
   foreach ($events as $event){
     //テキストを返信
