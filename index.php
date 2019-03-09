062cb9a28530cf0e496fc0e8f444846976f40899
n0fu24trifecta <trifecta131@gmail.com>
2019-03-10 00:46:00 +0900

    test2

--- index.php
+++ index.php
@@ -13,7 +13,8 @@
   //配列に格納された各イベントをループ処理
   foreach ($events as $event){
     //テキストを返信
-    $bot->replyText($event->getReplyToken(),'TextMessage');
+    //$bot->replyText($event->getReplyToken(),'TextMessage');
+    $replyTextMessage($bot,$event->getReplyToken(),'textmessage');
   }
 
   //テキストを返信。引数はLINEBOT、返信先、テキスト
