258c10c700251d2f8ed21389a94c4f72ddd3a312
n0fu24trifecta <trifecta131@gmail.com>
2019-03-16 07:17:36 +0900

    62ページ

--- index.php
+++ index.php
@@ -14,7 +14,21 @@
   foreach ($events as $event){
     //テキストを返信
     //$bot->replyText($event->getReplyToken(),'TextMessage');
-    replyTextMessage($bot,$event->getReplyToken(),'textmessage');
+    //replyTextMessage($bot,$event->getReplyToken(),'textmessage');
+    //BottonsTemplateを返信
+    replyBottonsTemplate($bot,
+      $event->getReplyToken(),
+      'お天気お知らせ - 今日の天気予報は晴れです',
+      'https://' . $_SERVER['HTTP_HOST'] . '/imgs/template.jpg',
+      'お天気お知らせ',
+      '今日の天気予報は晴れです',
+      new \LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder(
+        '明日の天気','tomorrow'),
+      new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder(
+        '週末の天気','weekend'),
+      new \LINE\LINEBot\TemplateActionBuilder\UrlTemplateActionBuilder(
+        'webで見る','http://google.jp')
+    );
   }
 
   //テキストを返信。引数はLINEBOT、返信先、テキスト
@@ -29,4 +43,24 @@
     }
   }
 
+  //BottonsTemplateを返信。引数は、LINEBot、返信先、代替テキスト、画像URL、タイトル、本文、アクション（可変長引数）
+  function replyBottonsTemplate($bot,$replyToken,$altenativeText,$imageUrl,$title,$text, ...$actions){
+    //actionを格納する配列
+    $actionArray = array();
+    //アクションすべてを追加
+    foreach($actions as $value){
+      array_push($actionArray,$value);
+    }
+    //TemplateMessageBuilderの引数は代替テキスト、ButttonTemplateBuilder
+    $builder = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder{
+      $altenativeText,
+      //ButttonTemplateBuilderの引数はタイトル、本文
+      //画像URL、アクションの配列
+      new \LINE\LINEBot\MessageBuilder\TemplateBuilder\BottonsTemplateBuilder($title,$text,$imageUrl,$actionArray)
+    };
+    $response = $bot->replyMessage($replyToken,$builder);
+    if(!$response->isSucceeded()){
+      error_log('Failed! '. $response->getHTTPStatus . ' ' . $response->getRawBody());
+    }
+  }
 ?>
